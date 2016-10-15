2b20b90dfd5f84f4fbe23c943ba14c5913da47ac
Toby Glover <william.glover@tufts.edu>
2016-10-15 09:02:45 -0400

    url regex

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^createEvent$', views.createEvent, name="createEvent"),
     url(r'^uploadImage$', views.uploadImage, name="uploadImage"),
     url(r'^getImages$', views.getImages, name="getImages"),
+    url(r'^getEventInfo$', views.getEventInfo, name="getEventInfo"),
     #url(r'^createNewPoll/(?P<userKey>[0-9a-f]+)/$', views.createNewPoll, name="createNewPoll"),
 ]
