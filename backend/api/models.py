8be81f3ee57183ec55b986224c81dca37279b7de
Toby Glover <william.glover@tufts.edu>
2016-10-14 22:03:27 -0400

    added frontend app to project

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Events(models.Model):
+	external_id = models.CharField(max_length=200, unique=True)
+	name = models.CharField(max_length=200)
+	description = models.CharField(max_length=200)
+	eventImage = models.CharField(max_length=200)
+	created = models.DateTimeField(auto_now=True)
+
+class Images(models.Model):
+	uri = models.CharField(max_length=200, unique=True)
+	event = models.ForeignKey(Events, on_delete=models.DO_NOTHING)
+	created = models.DateTimeField(auto_now=True)
